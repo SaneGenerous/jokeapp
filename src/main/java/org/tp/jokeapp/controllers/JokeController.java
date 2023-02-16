26de018f7113bdeca86ca4a502eba5400683122e
Sane Generous <karim2k11@gmail.com>
2023-02-16 12:31:40 +0000

    Chuck Norris Joke App using  spring.

---
+++ src/main/java/org/tp/jokeapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package org.tp.jokeapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.tp.jokeapp.service.JokeService;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+@RequestMapping({"/",""})
+    String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/org/tp/jokeapp/service/JokeService.java
@@ -0,0 +1,5 @@
+package org.tp.jokeapp.service;
+
+public interface JokeService {
+    String getJoke();
+}
